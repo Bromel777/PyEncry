69e729a82e825522ab0888b3adeab69e433f9b65
Ilya Oskin <ilya.arcadich@gmail.com>
2018-04-23 19:19:23 +0300

    AssetissuingDirective added.

--- encry/transaction.py
+++ encry/transaction.py
@@ -1,6 +1,16 @@
 from . import serialization
 
 
+class Script(serialization.JsonSerializable):
+    def __init__(self, serialized_script, complexity_score, script_fingerprint):
+        self.serialized_script = serialized_script
+        self.complexity_score = complexity_score
+        self.script_fingerprint = script_fingerprint
+
+    def json(self):
+        pass
+
+
 class Directive(serialization.JsonSerializable):
     """Base class for directives"""
 
@@ -16,14 +26,25 @@ class TransferDirective(Directive):
         pass
 
 
+class AssetIssuingDirective(Directive):
+    def __init__(self, script, amount, idx, symbol):
+        self.script = script
+        self.amount = amount
+        self.idx = idx
+        self.symbol = symbol
+
+    def json(self):
+        pass
+
+
 class Transaction(serialization.JsonSerializable):
-    def __init__(self, account_pk, fee, timestamp, signature, unlockers, directives):
+    def __init__(self, account_pk, fee, timestamp, unlockers, directives, signature=None):
         self.account_pk = account_pk
         self.fee = fee
         self.timestamp = timestamp
-        self.signature = signature
         self.unlockers = unlockers
         self.directives = directives
+        self.signature = signature
 
     def json(self):
         pass
